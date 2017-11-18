82d5047cd5ce6da78603dd94ee66f7a3a524cd8d
root <root@50318.s.time4vps.cloud>
2017-11-18 10:58:08 -0500

    20171804

--- solomotest.sh
+++ solomotest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -ne "Content-Type: text/html; charset=UTF-8\n\n"
-cat ./miner.log |grep -e "thread .*sh/s"|tail|awk '{print $1 $2 $5 }'
+cat ./miner.log |grep -e "thread .*sh/s"|tail|awk '{print $1" "$2" "$5 }'
 
 #END
 
