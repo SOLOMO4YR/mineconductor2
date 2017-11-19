4f12ff6c450e2e31ab2a391093d719cee3575ca6
root <root@50318.s.time4vps.cloud>
2017-11-19 05:35:52 -0500

    2017111900

--- mineconductor.sh
+++ mineconductor.sh
@@ -4,9 +4,9 @@ cpuamount=`cat /proc/cpuinfo |grep vendor_id|wc -l`
 hostname=`hostname|awk -F . '{print $1}'`
 
 if [ $cpuamount -eq 1 ]  ; then
-cpulimit -l 90 -- /root/cpuminer/minerd -a yescrypt -o stratum+tcp://bunnymining.work:19333 -u solomo.$hostname -p password -t 1 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
+cpulimit -l 90 -- /root/cpuminer/minerd -a yescrypt -o stratum+tcp://mining-zeny.mdpool.info:6961 -u solomo4yr.$hostname -p password -t 1 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
 else
-cpulimit -l 190 -- /root/cpuminer/minerd -a yescrypt -o stratum+tcp://bunnymining.work:19333 -u solomo.$hostname -p password -t 2 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
+cpulimit -l 190 -- /root/cpuminer/minerd -a yescrypt -o stratum+tcp://mining-zeny.mdpool.info:6961 -u solomo4yr.$hostname -p password -t 2 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
 fi
 sleep 2h
 shutdown -r now
