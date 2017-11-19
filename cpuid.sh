98835a4c22faf9a377988f4ea5dde1ec24110cc8
root <root@50318.s.time4vps.cloud>
2017-11-19 07:16:14 -0500

    2017111902

--- cpuid.sh
+++ cpuid.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo -ne "Content-Type: text/plain; charset=UTF-8\n\n"
+hostname|tr '\n' ' '
 tmp=`cat -ne /proc/cpuinfo |grep "model name"`
-echo $tmp|awk -F [:] '{printf $2":"}'
+echo $tmp|awk -F [:] '{printf ":"$2":"}'|sed "s/model name//g" 
 cat /proc/cpuinfo |grep vendor_id|wc -l
 
 #END
