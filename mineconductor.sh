bfd3c4bfadf8ec4c2dddd86975e2825f8b453d9e
root <root@50318.s.time4vps.cloud>
2017-11-19 17:28:04 -0500

    2017112000

--- mineconductor.sh
+++ mineconductor.sh
@@ -2,9 +2,14 @@
 
 cpuamount=`cat /proc/cpuinfo |grep vendor_id|wc -l`
 hostname=`hostname|awk -F . '{print $1}'`
-
+rand=`od -vAn -N4 -tu4 < /dev/random`
+rand=`expr $rand % 2`
 if [ $cpuamount -eq 1 ]  ; then
-cpulimit -l 90 -- /root/cpuminer/minerd -a yescrypt -o stratum+tcp://mining-zeny.mdpool.info:6961 -u solomo4yr.$hostname -p password -t 1 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
+	if [ $rand -eq 1 ] ; then
+		cpulimit -l 90 -- /root/cpuminer/minerd -a yescrypt -o stratum+tcp://mining-zeny.mdpool.info:6969 -u solomo4yr.$hostname -p password -t 1 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
+	else
+		cpulimit -l 90 -- /root/cpuminer/minerd -a yescrypt -o stratum+tcp://mining-zeny.mdpool.info:6960 -u solomo4yr.$hostname -p password -t 1 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
+	fi
 else
 cpulimit -l 190 -- /root/cpuminer/minerd -a yescrypt -o stratum+tcp://mining-zeny.mdpool.info:6961 -u solomo4yr.$hostname -p password -t 2 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
 fi
