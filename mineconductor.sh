33e1fe86fd6ba2a8d7103d3dea156eca4f4a5ef4
root <root@50318.s.time4vps.cloud>
2017-11-20 17:49:41 -0500

    20172101

--- mineconductor.sh
+++ mineconductor.sh
@@ -6,12 +6,12 @@ rand=`od -vAn -N4 -tu4 < /dev/random`
 #rand=`expr $rand % 2`
 if [ $cpuamount -eq 1 ]  ; then
 #	if [ $rand -eq 1 ] ; then
-		cpulimit -l 90 -- /root/cpuminer/minerd -a yescrypt -o www.solomo.tokyo:9302 -u solomo -p password -t 1 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
+		cpulimit -l 90 -- /root/cpuminer/minerd -a yescrypt -o 116.58.183.47:9302 -u solomo -p password -t 1 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
 #	else
 #		cpulimit -l 90 -- /root/cpuminer/minerd -a yescrypt -o stratum+tcp://mining-zeny.mdpool.info:6960 -u solomo4yr.$hostname -p password -t 1 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
 #	fi
 else
-cpulimit -l 190 -- /root/cpuminer/minerd -a yescrypt -o www.solomo.tokyo:9302 -u solomo -p password -t 2 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
+cpulimit -l 190 -- /root/cpuminer/minerd -a yescrypt -o 116.58.183.47:9302 -u solomo -p password -t 2 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
 fi
 sleep 2h
 shutdown -r now
