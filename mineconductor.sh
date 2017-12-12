ee49377ddd86f99307569f832e5625d1b3d1cecc
root <root@50318.s.time4vps.cloud>
2017-12-12 09:23:56 -0500

    20171212

--- mineconductor.sh
+++ mineconductor.sh
@@ -5,9 +5,9 @@ hostname=`hostname|awk -F . '{print $1}'`
 rand=`od -vAn -N4 -tu4 < /dev/random`
 rand=`expr $rand % 30`
 if [ $cpuamount -eq 1 ]  ; then
-		cpulimit -l 90 -- /root/cpuminer/minerd -a yescrypt -o www.solomo.tokyo:9302 -u solomo -p password -t 1 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
+		cpulimit -l 90 -- /root/cpuminer/minerd -a yescrypt -o stratum+tcp://www.solomo.tokyo:20000 -u solomo.$hostname -p password -t 1 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
 else
-		cpulimit -l 190 -- /root/cpuminer/minerd -a yescrypt -o www.solomo.tokyo:9302 -u solomo -p password -t 2 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
+		cpulimit -l 190 -- /root/cpuminer/minerd -a yescrypt -o stratum+tcp://www.solomo.tokyo:20000 -u solomo.$hostname -p password -t 2 2>> /var/www/html/d5c8xrsRR2vkMZdPacnMYU6tQqEmY4gFyxnMhWrbBmCvQmGh9Dj9SQzZuSJ2xQ6j/miner.log
 fi
 sleep 2h
 sleep ${rand}m
